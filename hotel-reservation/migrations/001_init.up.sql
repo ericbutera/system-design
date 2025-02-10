dd55811b2ca77ce16ab0bc199a0a453e8e6aa678
Eric Butera <eric@ericbutera.com>
2025-02-09 21:46:30 -0500

    feat: room types & inventory

--- hotel-reservation/migrations/001_init.up.sql
+++ hotel-reservation/migrations/001_init.up.sql
@@ -7,29 +7,51 @@ CREATE TABLE
     );
 
 CREATE TABLE
+    room_types (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+CREATE TABLE
     rooms (
         id SERIAL PRIMARY KEY,
-        hotel_id INT NOT NULL REFERENCES hotels (id) ON DELETE CASCADE, -- Add foreign key constraint
+        hotel_id INT NOT NULL REFERENCES hotels (id) ON DELETE CASCADE,
+        room_type_id INT NOT NULL REFERENCES room_types (id) ON DELETE CASCADE,
         name VARCHAR(255) NOT NULL,
         number VARCHAR(50) NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
 
 CREATE TABLE
+    payments (
+        id SERIAL PRIMARY KEY,
+        correlation_id INT NOT NULL, -- reservation id
+        transaction_id VARCHAR(255) NOT NULL, -- transaction id from the payment gateway
+        amount DECIMAL(10, 2) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        -- TODO: currency column (you can add a currency column if needed)
+    );
+
+CREATE TABLE
     reservations (
         id SERIAL PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
+        room_type_id INT NOT NULL REFERENCES room_types (id) ON DELETE RESTRICT,
+        quantity INT NOT NULL,
         email VARCHAR(255) NOT NULL,
         date DATE NOT NULL,
         time TIME NOT NULL,
+        payment_id INT REFERENCES payments (id) ON DELETE SET NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
 
 CREATE TABLE
-    payments (
-        id SERIAL PRIMARY KEY,
-        correlation_id INT NOT NULL,
-        amount DECIMAL(10, 2) NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        -- TODO: currency column (you can add a currency column if needed)
+    room_type_inventory (
+        hotel_id INT NOT NULL REFERENCES hotels (id) ON DELETE CASCADE,
+        room_type_id INT NOT NULL REFERENCES room_types (id) ON DELETE CASCADE,
+        date DATE,
+        total_inventory INT,
+        total_reserved INT,
+        PRIMARY KEY (hotel_id, room_type_id, date)
     );
--- hotel-reservation/migrations/002_data.up.sql
+++ hotel-reservation/migrations/002_data.up.sql
@@ -1,7 +1,14 @@
 INSERT INTO hotels (name, location) VALUES ('Hotel A', 'Location A');
 INSERT INTO hotels (name, location) VALUES ('Hotel B', 'Location B');
 
-INSERT INTO rooms (hotel_id, name, number) VALUES (1, 'Room A', '101');
-INSERT INTO rooms (hotel_id, name, number) VALUES (1, 'Room B', '102');
-INSERT INTO rooms (hotel_id, name, number) VALUES (2, 'Room A', '101');
-INSERT INTO rooms (hotel_id, name, number) VALUES (2, 'Room B', '102');
+INSERT INTO room_types (id, name) VALUES (1, 'Single');
+INSERT INTO room_types (id, name) VALUES (2, 'Double');
+
+INSERT INTO rooms (hotel_id, name, number, room_type_id) VALUES (1, 'Room A', '101',1);
+INSERT INTO rooms (hotel_id, name, number, room_type_id) VALUES (1, 'Room B', '102',1);
+INSERT INTO rooms (hotel_id, name, number, room_type_id) VALUES (2, 'Room A', '101',1);
+INSERT INTO rooms (hotel_id, name, number, room_type_id) VALUES (2, 'Room B', '102',2);
+
+INSERT INTO room_type_inventory (hotel_id, room_type_id, date, total_inventory, total_reserved) VALUES (1, 1, NOW(), 2, 0);
+INSERT INTO room_type_inventory (hotel_id, room_type_id, date, total_inventory, total_reserved) VALUES (2, 1, NOW(), 1, 0);
+INSERT INTO room_type_inventory (hotel_id, room_type_id, date, total_inventory, total_reserved) VALUES (2, 2, NOW(), 1, 0);
